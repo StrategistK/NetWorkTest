04fc30a301367728f40897c392a7aa3e38019b4c
strategist k <815034557@qq.com>
2016-11-22 20:28:44 +0800

    Http请求
    使用HttpClient

--- app/src/main/java/com/study/android/networktest/MainActivity.java
+++ app/src/main/java/com/study/android/networktest/MainActivity.java
@@ -8,11 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -46,42 +48,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.send_request) {
-            sendRequestWithHttpURLConnection();
+            sendRequestWithHttpClient();
         }
     }
 
-    private void sendRequestWithHttpURLConnection() {
-//        开启线程来发起网络请求
+    private void sendRequestWithHttpClient() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                HttpURLConnection connection = null;
                 try {
-                    URL url = new URL("http://www.bing.com");
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setConnectTimeout(8000);
-                    connection.setReadTimeout(8000);
-                    InputStream in = connection.getInputStream();
-//                    下面对获取到的输入流进行读取
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while((line = reader.readLine()) != null) {
-                        response.append(line);
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpGet httpGet = new HttpGet("http://www.bing.com");
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+                    if (httpResponse.getStatusLine().getStatusCode() == 200){
+                        HttpEntity entity = httpResponse.getEntity();
+                        String response = EntityUtils.toString(entity,"utf-8");
+                        Message message = new Message();
+                        message.what = SHOW_RESPONSE;
+                        message.obj = response.toString();
+                        mHandler.sendMessage(message);
                     }
-                    Message message = new Message();
-                    message.what = SHOW_RESPONSE;
-//                    将服务器返回的结果存到message中
-                    message.obj = response.toString();
-                    mHandler.sendMessage(message);
 
                 }catch (Exception e) {
                     e.printStackTrace();
-                }finally {
-                    if (connection != null){
-                        connection.disconnect();
-                    }
                 }
             }
         }).start();
